8720ca6adafcaa53549f6f622e1e3450782f1c5b
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-18 19:22:08 -0700

    update server

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { db } = require('./db/db');
 const { readdirSync } = require('fs')
 const app = express()
-const path = require('path');
+// const path = require('path');
 
 require('dotenv').config()
 
@@ -13,17 +13,13 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-if (process.env.NODE_ENV === 'production') {
-    //  *Set static folder up in production
-    app.use(express.static('https://expense-tracker-mk0mut9ad-geraldwaga92.vercel.app/'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'https://expense-tracker-mk0mut9ad-geraldwaga92.vercel.app/')));
-
-    //routes
-    // this will read the routes folder and all its contents
-    readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
-}
-
+//routes
+app.get("/", (req, res) => {
+    res.send({ message: "Hello World!" });
+});
+// this will read the routes folder and all its contents
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 
 const server = () => {
